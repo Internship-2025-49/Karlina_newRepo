a18213e5ad0126d4ee05ea4f4562704c1a87c330
Shita Zeny <shita0639@gmail.com>
2025-02-21 10:48:51 +0700

    feat: drizzle

---
+++ karlina-hono/prisma/src/test/project.test.ts
@@ -0,0 +1,15 @@
+// const express = require ('express');
+// const app = express();
+
+// app.post('/api/person', async (req, res) =>{
+
+// });
+
+// app.get('/api/person', async (req, res) =>{
+
+// });
+
+// app.delete('/api/person', async (req, res) =>{
+
+// });
+
